216bf0351c5e8dad5dc281c83fc21d4d55c19af5
Constantin Sazonov <113122196+JesusFreddie@users.noreply.github.com>
2024-12-20 11:33:11 +0500

    Сазонов Константин Финал точно

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CarController;
+use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RequestController;
+use App\Http\Middleware\Admin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,18 +19,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/', [MainController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('cars/create', [CarController::class, 'create'])->name('cars.create');
+    Route::post('cars/store', [CarController::class, 'store'])->name('cars.store');
+
+    Route::get('requests/create', [RequestController::class, 'create'])->name('requests.create');
+    Route::post('requests/store', [RequestController::class, 'store'])->name('requests.store');
+});
+
+Route::middleware((Admin::class))->group(function(){
+    Route::get('/admin', [AdminController::class, 'index']) -> name('admin.index');
+    Route::put('/requests/update/', [RequestController::class, 'update'])->name('requests.update');
+    // Route::put('/update', [ReportController::class, 'update'])->name('reports.update');
 });
 
 require __DIR__.'/auth.php';
